c4012ebe71c2510f83e19e9c2ba2bd15bb79a270
Maliek Davis <maliekjdavis24@gmail.com>
2023-08-29 02:08:57 -0400

    index animation fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -201,9 +201,9 @@ export default function Home() {
         {isWorkHoriBg && hovered ?
               <motion.div 
               className='mb-3 '
-              initial={{opacity:0, y:-100, scale: .5}}
-              animate={{opacity:1, y:0, scale: 1}}
-              transition={{duration: 0.75}}
+              initial={{opacity:0.1,}}
+              animate={{opacity:1}}
+              transition={{duration: 1}}
               >
           <Typography component='div' variant='h1' sx={{fontFamily: 'rajdhani', color:blue[500], textShadow: '2px 2px #fff', fontSize: {xs: '5rem'}}} className=' sm:text-center'>
           Hello, I&apos;m <motion.span 
@@ -284,8 +284,8 @@ export default function Home() {
       {isWorkHoriBg  && hovered ?
         <motion.div 
         className='mb-3 lg:px-24'
-        initial={{opacity:0, y:-100}}
-        animate={{opacity:1, y:0}}
+        initial={{opacity:0}}
+        animate={{opacity:1}}
         transition={{duration: 1, delay:0.25}}
         >
           <Typography variant='h5' sx={{color:grey[50], fontFamily: 'rajdhani', }} component='div' className=' md:px-24 sm:text-center ' >
@@ -355,34 +355,48 @@ export default function Home() {
       }
 
       {isWorkHoriBg  && hovered ?
-        <motion.div 
-        className='flex flex-row items-center justify-center gap-3'
+          <>
+          <motion.div 
+        className='items-center justify-center gap-3'
         initial={{opacity:0, scale:.25}}
-        animate={{ opacity:1, scale: 1 }}
-        transition={{ duration: .75, delay:1 }}
+        animate={{ opacity:1, scale: [1] }}
+        transition={{ ease: "easeOut", duration: .75, delay:1 }}
         >
-        <motion.div
-         onHoverEnd={() => {setHovered(false); setColor(false)}}  
-        >
-          <Link href="/work">
-            <Button  onMouseEnter={() => {
-                handleBackground('professional');
-                setHovered(true); setColor(true) // Set hover status to true
-              }}
-              variant='contained' sx={{fontFamily: 'rajdhani',}}>
-                My Work
-            </Button>
-          </Link>
-        </motion.div>
-        <motion.div
-           onHoverEnd={() => {setHovered(false); setColor(false)}}
-        >
-          <Link href="/about"><Button onMouseEnter={() => {
-                handleBackground('personal');
-                setHovered(true); setColor(true) // Set hover status to true
-              }} variant='outlined' sx={{fontFamily: 'kodchasan', borderColor: grey[50], color: grey[50]}}>About Me</Button></Link>
-        </motion.div>
-        </motion.div> : null
+            <div className='w-full'>
+              <Typography variant='h6' component='div' sx={{width:'100%', fontFamily: 'kodchasan', color: grey[500]}} className='mb-1'>
+                Hover or Tap&Hold, I&apos;m Interactive! 
+              </Typography>
+            </div></motion.div>
+            <motion.div 
+              className='flex flex-row items-center justify-center gap-3'
+              initial={{opacity:0.1}}
+              animate={{ opacity:1}}
+              transition={{ duration: 1, delay:1 }}
+              >
+                <motion.div
+                onHoverEnd={() => {setHovered(false); setColor(false)}}  
+                >
+                  <Link href="/work">
+                    <Button  onMouseEnter={() => {
+                        handleBackground('professional');
+                        setHovered(true); setColor(true) // Set hover status to true
+                      }}
+                      variant='contained' sx={{fontFamily: 'rajdhani',}}>
+                        My Work
+                    </Button>
+                  </Link>
+                </motion.div>
+                <motion.div
+                  onHoverEnd={() => {setHovered(false); setColor(false)}}
+                >
+                  <Link href="/about"><Button onMouseEnter={() => {
+                        handleBackground('personal');
+                        setHovered(true); setColor(true) // Set hover status to true
+                      }} variant='outlined' sx={{fontFamily: 'kodchasan', borderColor: grey[50], color: grey[50]}}>About Me</Button></Link>
+                </motion.div>
+              </motion.div>      
+          </>
+        : null
 
       }
       {isWorkVertBg && hovered ?
@@ -418,34 +432,48 @@ export default function Home() {
 
       }
       {isAboutHoriBg && hovered ?
-              <motion.div 
-      className='flex flex-row items-center justify-center gap-3'
-      initial={{opacity:0, scale:.25}}
-      animate={{ opacity:1, scale: 1 }}
-      transition={{ ease: "easeOut", duration: .75, delay:1 }}
-      >
-        <motion.div
-         onHoverEnd={() => {setHovered(false); setColor(false)}}  
-        >
-          <Link href="/work">
-            <Button  onMouseEnter={() => {
-                handleBackground('professional');
-                setHovered(true); setColor(true) // Set hover status to true
-              }}
-              variant='contained' sx={{fontFamily: 'rajdhani',}}>
-                My Work
-            </Button>
-          </Link>
-        </motion.div>
-        <motion.div
-           onHoverEnd={() => {setHovered(false); setColor(false)}}
-        >
-          <Link href="/about"><Button onMouseEnter={() => {
-                handleBackground('personal');
-                setHovered(true); setColor(true) // Set hover status to true
-              }} variant='outlined' sx={{fontFamily: 'kodchasan', borderColor: grey[50], color: grey[50]}}>About Me</Button></Link>
-        </motion.div>
-      </motion.div> : null
+        <>
+          <motion.div 
+          className='items-center justify-center gap-3'
+          initial={{opacity:0, scale:.25}}
+          animate={{ opacity:1, scale: [1] }}
+          transition={{ ease: "easeOut", duration: .75, delay:1 }}
+          >
+            <div className='w-full'>
+              <Typography variant='h6' component='div' sx={{width:'100%', fontFamily: 'kodchasan', color: grey[500]}} className='mb-1'>
+                Hover or Tap&Hold, I&apos;m Interactive! 
+              </Typography>
+            </div>
+          </motion.div>
+                <motion.div 
+                className='flex flex-row items-center justify-center gap-3'
+                initial={{opacity:0, scale:.25}}
+                animate={{ opacity:1, scale: 1 }}
+                transition={{ ease: "easeOut", duration: .75, delay:1 }}
+                >
+                  <motion.div
+                  onHoverEnd={() => {setHovered(false); setColor(false)}}  
+                  >
+                    <Link href="/work">
+                      <Button  onMouseEnter={() => {
+                          handleBackground('professional');
+                          setHovered(true); setColor(true) // Set hover status to true
+                        }}
+                        variant='contained' sx={{fontFamily: 'rajdhani',}}>
+                          My Work
+                      </Button>
+                    </Link>
+                  </motion.div>
+                  <motion.div
+                    onHoverEnd={() => {setHovered(false); setColor(false)}}
+                  >
+                    <Link href="/about"><Button onMouseEnter={() => {
+                          handleBackground('personal');
+                          setHovered(true); setColor(true) // Set hover status to true
+                        }} variant='outlined' sx={{fontFamily: 'kodchasan', borderColor: grey[50], color: grey[50]}}>About Me</Button></Link>
+                  </motion.div>
+                </motion.div>      
+        </> : null
 
       }
       {isAboutVertBg && hovered ?
