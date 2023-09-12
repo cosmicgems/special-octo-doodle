494c938ba8954bda3c0110bde9b8d1ac8c9de585
Maliek Davis <maliekjdavis24@gmail.com>
2023-09-11 21:43:29 -0400

    new env variable added to vercel

--- src/components/Home Page/CollageBackground.tsx
+++ src/components/Home Page/CollageBackground.tsx
@@ -2,6 +2,7 @@ import { Box, ImageList, ImageListItem, } from '@mui/material'
 import React, { useState } from 'react'
 import { motion } from "framer-motion"
 import { images } from '../../assets/images'
+import Image from 'next/image'
 
 const CollageBackground = () => {
     function shuffleArray(array:any) {
@@ -29,58 +30,70 @@ const CollageBackground = () => {
         setLoadedImages((prevLoadedImages) => [...prevLoadedImages, imageIndex]);
     };
 
-    return (
-        <div className="video-container">
-            <Box sx={{ width: "100%", height: "100vh", }}>
-                <ImageList className='sm:hidden' variant="masonry" cols={4} gap={2}>
-                    {shuffledImages.map((item, index) => (
-                    <ImageListItem key={item.image}>
-                    {/* Display low-quality image initially */}
-                    <img
-                        src={`${item.image}?w=100&fit=crop&auto=format`}  // Use a lower-quality image here
-                        alt='Images of me and my family is a quilt'
-                        style={{ filter: loadedImages.includes(index) ? 'none' : 'blur(20px)' }}
-                    />
-
-                    {/* Load high-quality image in the background */}
-                    <img
-                        src={`${item.image}?w=248&fit=crop&auto=format`}  // Use the high-quality image here
-                        alt='Images of me and my family is a quilt'
-                        style={{ display: 'none' }}
-                        onLoad={() => handleImageLoad(index)}  // Mark the image as loaded
-                    />
-                    </ImageListItem>
-                    ))}
-                </ImageList>
-                <ImageList className='hidden sm:block' variant="masonry" cols={12} gap={4}>
-                {shuffledImages.map((item, index) => (
-                    <ImageListItem key={item.image}>
-                    {/* Display low-quality image initially */}
-                    <img
-                        src={`${item.image}?w=100&fit=crop&auto=format`}  // Use a lower-quality image here
-                        alt='Images of me and my family is a quilt'
-                        style={{ filter: loadedImages.includes(index) ? 'none' : 'blur(20px)' }}
-                    />
-
-                    {/* Load high-quality image in the background */}
-                    <img
-                        src={`${item.image}?w=248&fit=crop&auto=format`}  // Use the high-quality image here
-                        alt='Images of me and my family is a quilt'
-                        style={{ display: 'none' }}
-                        onLoad={() => handleImageLoad(index)}  // Mark the image as loaded
-                    />
-                    </ImageListItem>
-                ))}
-                </ImageList>
-                <motion.div 
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: .01 }}
-                    transition={{ duration: 0.75 }} 
-                    className="overlay2"
+    return ( 
+    <div className="video-container">
+        <Box sx={{ width: '100%', height: '100vh' }}>
+        <ImageList className='sm:hidden' variant="masonry" cols={4} gap={2}>
+            {shuffledImages.map((item, index) => (
+            <ImageListItem key={item.image}>
+                {/* Display low-quality image initially */}
+                <Image
+                src={`${item.image}?w=100&fit=crop&auto=format`} // Use a lower-quality image here
+                alt='Images of me and my family is a quilt'
+                layout="responsive" // Use responsive layout for the image
+                width={100}
+                height={100} // Set the dimensions you need for the low-quality image
+                style={{ filter: loadedImages.includes(index) ? 'none' : 'blur(20px)' }}
                 />
-            </Box>
-        </div>
-    )
+
+                {/* Load high-quality image in the background */}
+                <Image
+                src={`${item.image}?w=248&fit=crop&auto=format`} // Use the high-quality image here
+                alt='Images of me and my family is a quilt'
+                layout="responsive" // Use responsive layout for the image
+                width={248}
+                height={248} // Set the dimensions you need for the high-quality image
+                style={{ display: 'none' }}
+                onLoad={() => handleImageLoad(index)} // Mark the image as loaded
+                />
+            </ImageListItem>
+            ))}
+        </ImageList>
+        <ImageList className='hidden sm:block' variant="masonry" cols={12} gap={4}>
+            {shuffledImages.map((item, index) => (
+            <ImageListItem key={item.image}>
+                {/* Display low-quality image initially */}
+                <Image
+                src={`${item.image}?w=100&fit=crop&auto=format`} // Use a lower-quality image here
+                alt='Images of me and my family is a quilt'
+                layout="responsive" // Use responsive layout for the image
+                width={100}
+                height={100} // Set the dimensions you need for the low-quality image
+                style={{ filter: loadedImages.includes(index) ? 'none' : 'blur(20px)' }}
+                />
+
+                {/* Load high-quality image in the background */}
+                <Image
+                src={`${item.image}?w=248&fit=crop&auto=format`} // Use the high-quality image here
+                alt='Images of me and my family is a quilt'
+                layout="responsive" // Use responsive layout for the image
+                width={248}
+                height={248} // Set the dimensions you need for the high-quality image
+                style={{ display: 'none' }}
+                onLoad={() => handleImageLoad(index)} // Mark the image as loaded
+                />
+            </ImageListItem>
+            ))}
+        </ImageList>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0.01 }}
+            transition={{ duration: 0.75 }}
+            className="overlay2"
+        />
+        </Box>
+    </div>
+    );
 }
 
 export default CollageBackground
